5550e7b180ff994b9f298b665d4081f03723669e
manavbrar <manavbrar@gmail.com>
2021-04-23 08:45:39 -0700

    1) wire navigation to from instructions to list

--- starter/app/src/main/java/com/udacity/shoestore/screens/ui/instructions/InstructionsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/ui/instructions/InstructionsFragment.kt
@@ -1,12 +1,17 @@
 package com.udacity.shoestore.screens.ui.instructions
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.InstructionsFragmentBinding
+import com.udacity.shoestore.screens.ui.instructions.InstructionsFragmentDirections.Companion.actionInstructionsFragmentToScrollingFragment
 
 class InstructionsFragment : Fragment() {
 
@@ -15,18 +20,28 @@ class InstructionsFragment : Fragment() {
     }
 
     private lateinit var viewModel: InstructionsViewModel
+    private lateinit var binding: InstructionsFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.instructions_fragment, container, false)
+        binding = DataBindingUtil.inflate<InstructionsFragmentBinding>(inflater, R.layout.instructions_fragment,
+                container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(InstructionsViewModel::class.java)
-        // TODO: Use the ViewModel
+        this.also { binding.lifecycleOwner = it }
+        binding.instructionVewModel = viewModel
+        viewModel.hasSelectedList.observe(viewLifecycleOwner, Observer { hasClickedInstructions ->
+            if (hasClickedInstructions) {
+                findNavController().navigate(actionInstructionsFragmentToScrollingFragment())
+                viewModel.hasCompletedList()
+            }
+        })
     }
 
 }
--- starter/app/src/main/java/com/udacity/shoestore/screens/ui/instructions/InstructionsViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/ui/instructions/InstructionsViewModel.kt
@@ -1,7 +1,21 @@
 package com.udacity.shoestore.screens.ui.instructions
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class InstructionsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private var _selectedList = MutableLiveData<Boolean>()
+    val hasSelectedList: LiveData<Boolean>
+        get() = _selectedList
+
+    init{
+         hasCompletedList()
+    }
+    fun onSelectedList() {
+        true.also { _selectedList.value = it }
+    }
+    fun hasCompletedList() {
+        false.also { _selectedList.value = it }
+    }
 }
